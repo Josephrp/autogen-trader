fd6393327159a67675a9b784f75483f5da2ae6ef
aa-wong <waaron.wong@gmail.com>
2023-11-02 16:31:27 -0400

    Alpaca api class working with order functions

--- example.py
+++ example.py
@@ -5,4 +5,12 @@ load_dotenv()
 
 client = AlpacaClient(os.getenv('ALPACA_API_KEY'), os.getenv('ALPACA_API_SECRET'))
 
-print(client.buying_power)
+print(client.buying_power())
+print(client.gain_loss())
+# print(client.get_all_assets())
+print(client.get_all_positions())
+
+# print(client.create_buy_market_order("SPY", 1))
+# print(client.get_open_position("SPY"))
+print(client.get_all_positions())
+print(client.get_all_open_orders())
